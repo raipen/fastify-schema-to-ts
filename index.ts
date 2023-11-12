e0a515cddeaf256935b17e0853baeadf9e77c7fd
raipen <raipendalk@gmail.com>
2023-11-13 06:29:50 +0900

    feat: implement "fastify schema to interface"

--- index.ts
+++ index.ts
@@ -1,4 +1,30 @@
-export function foo(): string {
-  console.log("foo");
-  return "foo";
-}
+import { FromSchema, JSONSchema } from 'json-schema-to-ts';
+
+export type SchemaToInterface<
+  T extends {
+    body?: JSONSchema;
+    querystring?: JSONSchema;
+    params?: JSONSchema;
+    headers?: JSONSchema;
+    response?: { [key: string]: JSONSchema };
+  },
+  Option extends [{ pattern: unknown; output: unknown }] | false = false
+> = {
+  Body: T['body'] extends JSONSchema
+  ? FromSchema<T['body'], { deserialize: Option }>
+  : unknown;
+  Querystring: T['querystring'] extends JSONSchema
+  ? FromSchema<T['querystring'], { deserialize: Option }>
+  : unknown;
+  Params: T['params'] extends JSONSchema
+  ? FromSchema<T['params'], { deserialize: Option }>
+  : unknown;
+  Headers: T['headers'] extends JSONSchema
+  ? FromSchema<T['headers'], { deserialize: Option }>
+  : unknown;
+  Reply: {
+    [key in keyof T['response']]: T['response'][key] extends JSONSchema
+    ? FromSchema<T['response'][key],{ deserialize: Option }>
+    : unknown;
+  };
+};
